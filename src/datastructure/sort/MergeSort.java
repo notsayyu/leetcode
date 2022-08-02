f96ccab7227d0eef2be6d7531b8258a90b42a9e9
notsayyu <1007393287@qq.com>
2022-08-02 10:28:58 +0800

    自己重写归并排序

--- src/datastructure/sort/MergeSort.java
+++ src/datastructure/sort/MergeSort.java
@@ -21,6 +21,63 @@ public class MergeSort implements Sorter {
         sortHelper(arr, 0, arr.length - 1, tmp);
     }
 
+
+    public static void mySort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        int[] tmp = new int[arr.length];
+        mySortHelper(arr, 0, arr.length - 1, tmp);
+    }
+
+
+    private static void mySortHelper(int[] arr, int start, int end, int[] tmp) {
+        //如果分到单个了，就结束
+        if (start >= end) {
+            return;
+        }
+
+        //分成两半，递归处理每一半
+        int mid = (start + end) / 2;
+        mySortHelper(arr, start, mid, tmp);
+        mySortHelper(arr, mid + 1, end, tmp);
+        myMerge(arr, start, mid, mid + 1, end, tmp);
+    }
+
+    /**
+     * 把分到最后的两半进行排序合并
+     */
+    private static void myMerge(int[] arr, int s1, int e1, int s2, int e2, int[] tmp) {
+        //挨个比较两段的数值，小的排序填充到tmp
+        int start = s1;
+        int k = 0;
+
+        while (s1 <= e1 && s2 <= e2) {
+            if (arr[s1] < arr[s2]) {
+                tmp[k++] = arr[s1];
+                s1++;
+            } else {
+                tmp[k++] = arr[s2];
+                s2++;
+            }
+        }
+
+        while (s1 <= e1) {
+            tmp[k++] = arr[s1];
+            s1++;
+        }
+
+        while (s2 <= e2) {
+            tmp[k++] = arr[s2];
+            s2++;
+        }
+
+        if (k >= 0) {
+            System.arraycopy(tmp, 0, arr, start, k);
+        }
+    }
+
+
     public void sortHelper(int[] arr, int start, int end, int[] tmp) {
         if (start >= end) {
             return;
@@ -61,9 +118,9 @@ public class MergeSort implements Sorter {
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 7, 1, 3, 9, 0, 5};
-        Sorter sorter = new MergeSort();
-        sorter.sort(arr);
+        int[] arr = {4, 7, 1, 3, 9, 0, 8, 5, 20};
+//        Sorter sorter = new MergeSort();
+        mySort(arr);
         for (int i : arr) {
             System.out.println(i);
         }
